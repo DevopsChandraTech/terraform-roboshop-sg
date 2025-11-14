85c1f9e6804c97b2dfe59c1c37886e2ef177dc1a
DevOpsChandraTech <devopschandra42@gmail.com>
2025-11-14 20:31:29 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,17 +1,6 @@
-resource "aws_security_group" "sg" {
+resource "aws_security_group" "main" {
   name        = var.sg_name
   vpc_id      = var.vpc_id
-
-tags = merge (
-    local.common_tags,
-    var.sg_tags,
-    {
-      Name = "${local.common_name_suffix}-${var.sg_name}"
-    }
-  )
-}
-
-resource "aws_security_group" "sg_outbound" {
 
   egress {
     from_port        = 0
@@ -20,4 +9,13 @@ resource "aws_security_group" "sg_outbound" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+
+  tags = merge (
+    local.common_tags,
+    var.sg_tags,
+    {
+      Name = "${local.common_name_suffix}-${var.sg_name}"
+    }
+  )
 }
+
